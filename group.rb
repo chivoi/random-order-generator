5bc23c88d30d2d1de7a14620e55e547466fdf919
chivoi <a.lastovirya@gmail.com>
2020-12-04 11:57:02 +1100

    implemented and tested add_name Group instance method

--- group.rb
+++ group.rb
@@ -1,9 +1,15 @@
+
+
 class Group
-  attr_reader :name, :names_array
+  attr_reader :name, :names_array, :file_path
   
   def initialize(name, file_path)
     @name = name
     @names_array = []
     @file_path = file_path
+  end
+
+  def add_name(name)
+    return @names_array.push(name)
   end
 end
--- spec/group_spec.rb
+++ spec/group_spec.rb
@@ -2,7 +2,7 @@ require_relative ("../group")
 
 describe 'group' do
   before(:each) do
-    @group = Group.new("Test group")
+    @group = Group.new("Test group", "./groups/test-group.txt")
   end
 
   it 'should be an instance of a Group' do
@@ -21,6 +21,20 @@ describe 'group' do
     expect(@group.file_path).to be_an_instance_of(String)
   end
 
+  it 'should have a file path that matches the file directory' do
+    expect(@group.file_path).to match(/^\.{1}\/groups\/\S+/)
+  end
 
+  describe '.add_name' do
+    it "should be defined" do
+      expect(defined? @group.add_name).to eq("method")
+    end
+
+    it "should increase the length of @names_array" do
+      length = @group.names_array.length
+      expect(@group.add_name("Alice").length).to be length+1
+    end
+
+  end
 
 end
