904943dcb3ba13aff5760c19135e5660f329e33c
chivoi <a.lastovirya@gmail.com>
2020-12-04 12:54:48 +1100

    changed initialize on Group to load array from path_to_array

--- group.rb
+++ group.rb
@@ -5,8 +5,8 @@ class Group
   
   def initialize(name, file_path)
     @name = name
-    @names_array = []
     @file_path = file_path
+    @names_array = self.path_to_array || []
   end
 
   def add_name(name)
@@ -14,8 +14,15 @@ class Group
   end
 
   def to_s
-    return "Name: #{@name} Members: #{@names_array.sort}"
+    return "Name: #{@name} \nMembers: #{@names_array.sort}"
   end
+
+  def path_to_array
+    return File.readlines(@file_path).map{|name| name.strip}
+  end
+
+
+
 end
 
 test_group = Group.new("Test group", "./groups/test-group.txt")
